bacf35024f56de730b2f6041510712f82a9cbabe
rahkumar27 <rahkumar27@publicisgroupe.net>
2023-05-09 20:18:32 +0530

    updated mongodb connection with await

--- server/server.js
+++ server/server.js
@@ -4,12 +4,11 @@ import mongoose from 'mongoose';
 const PORT = 4000;
 const app = express();
 
-mongoose
-  .connect(
-    'mongodb+srv://rahulraj90:Admin123@expensor-app.qihb9hj.mongodb.net/?retryWrites=true&w=majority'
-  )
-  .then(() => console.log('MongoDB connected successfully!'))
-  .catch((err) => console.error(err));
+// MongoDb connection
+await mongoose.connect(
+  'mongodb+srv://rahulraj90:Admin123@expensor-app.qihb9hj.mongodb.net/?retryWrites=true&w=majority'
+);
+console.log('MongoDB connected successfully!');
 
 app.get('/', (req, res) => {
   res.send(`Server started at PORT: ${PORT}`);
