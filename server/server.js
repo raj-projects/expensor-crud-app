297d0906d7d2d65f43a1d00c61f96706edd602fe
rahkumar27 <rahkumar27@publicisgroupe.net>
2023-05-09 20:14:53 +0530

    mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const PORT = 4000;
 const app = express();
 
+mongoose
+  .connect(
+    'mongodb+srv://rahulraj90:Admin123@expensor-app.qihb9hj.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => console.log('MongoDB connected successfully!'))
+  .catch((err) => console.error(err));
+
 app.get('/', (req, res) => {
-    res.send(`Server started at PORT: ${PORT}`);
+  res.send(`Server started at PORT: ${PORT}`);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running at PORT: ${PORT}`);
+  console.log(`Server running at PORT: ${PORT}`);
 });
-
