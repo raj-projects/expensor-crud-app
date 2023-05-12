12b3741e9fea599f76e2ddd9457bb77ff340b3db
rahkumar27 <rahkumar27@publicisgroupe.net>
2023-05-12 10:32:30 +0530

    add transaction routes

--- server/server.js
+++ server/server.js
@@ -1,40 +1,26 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import Transaction from './models/Transaction.js'
+import mongoose from 'mongoose';
+import TransactionApi from './routes/TransactionsApi.js';
 
 const PORT = 4000;
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send(`Server started at PORT: ${PORT}`);
+});
+
+app.use('/', TransactionApi);
 
 // MongoDb connection
 await mongoose.connect(
   'mongodb+srv://rahulraj90:Admin123@expensor-app.qihb9hj.mongodb.net/?retryWrites=true&w=majority'
 );
 console.log('MongoDB connected successfully!');
-
-app.get('/', (req, res) => {
-  res.send(`Server started at PORT: ${PORT}`);
-});
-
-app.get('/transaction', async (req, res) => {
-  const transaction = await Transaction.find({}).sort({ createdAt: -1 });
-  res.json({ data: transaction });
-});
-
-app.post('/transaction', async (req, res) => {
-  const { amount, description, date } = req.body;
-  const transaction = new Transaction({
-    amount,
-    description,
-    date
-  });
-  await transaction.save();
-  res.json({ message: 'Success' });
-});
 
 app.listen(PORT, () => {
   console.log(`Server running at PORT: ${PORT}`);
